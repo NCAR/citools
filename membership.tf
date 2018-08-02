a770e7411e5161938a24cda08147854a13db29ff
George Williams <gwilliam@ucar.edu>
2018-08-02 12:45:00 -0600

    people-sync auto-update

--- membership.tf
+++ membership.tf
@@ -1,35 +1,35 @@
 resource "github_membership" "membership_for_datvance" {
-  username: "datvance"
-  role: "admin"
+  username = "datvance"
+  role = "admin"
 }
 
 resource "github_membership" "membership_for_durist" {
-  username: "durist"
-  role: "admin"
+  username = "durist"
+  role = "admin"
 }
 
 resource "github_membership" "membership_for_geinosky" {
-  username: "geinosky"
-  role: "admin"
+  username = "geinosky"
+  role = "admin"
 }
 
 resource "github_membership" "membership_for_gwilliam-ucar-edu" {
-  username: "gwilliam-ucar-edu"
-  role: "admin"
+  username = "gwilliam-ucar-edu"
+  role = "admin"
 }
 
 resource "github_membership" "membership_for_jasoncunning" {
-  username: "jasoncunning"
-  role: "admin"
+  username = "jasoncunning"
+  role = "admin"
 }
 
 resource "github_membership" "membership_for_jdaves-ucar" {
-  username: "jdaves-ucar"
-  role: "admin"
+  username = "jdaves-ucar"
+  role = "admin"
 }
 
 resource "github_membership" "membership_for_wilhelmi" {
-  username: "wilhelmi"
-  role: "admin"
+  username = "wilhelmi"
+  role = "admin"
 }
 
